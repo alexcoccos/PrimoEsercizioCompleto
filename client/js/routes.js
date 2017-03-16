429969020a504e05642d5025d66c3699cce3acdf
Alex Contursi <contursi.axel@gmail.com>
2017-03-16 18:40:26 +0100

    finto prima chiamata al server

--- client/js/routes.js
+++ client/js/routes.js
@@ -1,8 +1,11 @@
 angular.module('app').config(function($stateProvider,$urlRouterProvider){
 
-  $stateProvider.state('/',{
-    url:'/',
-    templateUrl: 'todo/todo.template.html',
+  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.when('/','client')
+
+  $stateProvider.state('/client',{
+    url:'/client',
+    templateUrl: 'js/todo/todo.template.html',
     controller: 'TodoCtrl'
   });
 });
